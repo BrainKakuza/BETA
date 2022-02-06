cc5aa2e3ec040f0fdbeaddff9323084a52e6ff5d
BrainKakuza <brainditthakit@gmail.com>
2022-02-06 13:09:01 +0800

    Input Files

---
+++ C++ Programming/Programs/Project/Test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using std::cout;
+using std::string;
+using std::endl;
+using std::cin;
+
+int main(){
+
+    int i;
+
+    cin >> i;
+    cout << i;
+    
+
+    return 0;
+}
